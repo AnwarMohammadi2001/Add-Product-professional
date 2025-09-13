9633e3536989da99048a2fbf42142b29d68c542e
Anwar Mohammadi <anwarmohammadi1390@gmail.com>
2025-09-13 10:50:32 +0430

    use useContext and read some data from API

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import NavbarNew from "../components/NavbarNew";
 // import Footer from "../components/Footer";
 
